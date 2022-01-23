ef6f7f05bf108a76a070cf514566dbf955f33222
t <simp4t7@gmail.com>
2022-01-23 06:02:59 +0000

    more actions

--- gh_actions_init.sh
+++ gh_actions_init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo $ls
+ls
 sqlite3 db/movie_site.db '.quit'
 sqlite3 db/movie_site.db < db/clear_table.sql
 sqlite3 db/movie_site.db < db/db_setup.sql
 touch .env
 echo 'RUST_LOG=info' >> .env
-echo 'DATABASE_URL=sqlite:///$PWD/db/movie_site.db' >> .env
+echo 'DATABASE_URL=sqlite:///$PWD/../db/movie_site.db' >> .env
 echo 'DEV_SECRET="Umm, just a secret for testing and stuff, but Ill delete it later"' >> .env
 echo 'ACCESS_TOKEN_EXP=180000' >> .env
 echo 'REFRESH_TOKEN_EXP=6000000' >> .env
