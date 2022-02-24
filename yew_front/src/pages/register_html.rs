d047c04ac60cc9ccfc1c6fb98e7594e7d8101b78
t <simp4t7@gmail.com>
2022-02-24 04:50:43 +0000

    register page css

--- yew_front/src/pages/register_html.rs
+++ yew_front/src/pages/register_html.rs
@@ -29,78 +29,56 @@ impl Register {
     }
     pub fn register_html(&self, ctx: &Context<Self>) -> Html {
         html! {
-        <div>
-            <div>
-                <div class="container field column is-3">
-                <h1> {"REGISTER"} </h1>
-                  <p class="control has-icons-left">
-                    <input class="input" type="email" placeholder="Email"/>
+            <div class="has-background-grey-lighter">
+            <div class="container column is-6 is-centered">
+            <div class="card">
+                <p class="is-size-4 has-text-centered pt-3 mt-3">{"Register"}</p>
+                <div class="container field column is-9">
+                    <p class="control has-icons-left">
+                    <input class="input" type="email" placeholder="Email"
+                    maxlength=50
+                    oninput={ctx.link().callback(RegisterMsg::SetUsername)}/>
                     <span class="icon is-small is-left">
                     <i class="material-icons">{"email"}</i>
                     </span>
-                    <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                    </span>
                     </p>
-                    </div>
-                <div class="container field column is-3">
+                </div>
+                <div class="container field column is-9">
                   <p class="control has-icons-left">
-                    <input class="input" type="password" placeholder="Password"/>
+                    <input class="input" type="password" placeholder="Password"
+                    maxlength=50
+                    oninput={ctx.link().callback(RegisterMsg::SetPassword)}/>
                     <span class="icon is-small is-left">
                     <i class="material-icons">{"lock"}</i>
                     </span>
-                    <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                    </span>
                     </p>
-                    </div>
-                <div class="container field column is-3">
+                </div>
+                <div class="container field column is-9">
                   <p class="control has-icons-left">
-                    <input class="input" type="password" placeholder="Repeat Password"/>
+                    <input class="input" type="password" placeholder="Repeat Password"
+                    maxlength=50
+                    oninput={ctx.link().callback(RegisterMsg::ConfirmPassword)}/>
                     <span class="icon is-small is-left">
                     <i class="material-icons">{"lock"}</i>
                     </span>
-                    <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                    </span>
-                    </p>
-                    </div>
-                <input
-                    class="login_user_name"
-                    placeholder="Username"
-                    maxlength=50
-                    oninput={ctx.link().callback(RegisterMsg::SetUsername)}
-                />
-            </div>
-            <div>
-                <input
-                    type="password"
-                    class="login_user_name"
-                    placeholder="Password"
-                    maxlength=50
-                    oninput={ctx.link().callback(RegisterMsg::SetPassword)}
-                />
+                </p>
+                </div>
+                <div class="container field column is-9">
+                <button
+                    class="button is-primary"
+                    onclick={ctx.link().callback(|_| RegisterMsg::RegisterUser)}>
+                    { "Register" }
+                </button>
+                </div>
+                <div class="container column is-6 is-centered">
+                </div>
+
                 <p> {self.get_password_strength_text()} </p>
-            </div>
-            <div>
-                <input
-                    type="password"
-                    class="login_user_name"
-                    placeholder="Repeat password"
-                    maxlength=50
-                    oninput={ctx.link().callback(RegisterMsg::ConfirmPassword)}
-                />
                 <p> {self.repeated_password_matching_text()} </p>
+                <p> {format!("Register error: {:?}", self.error.clone())} </p>
             </div>
-            <div>
-            <button
-                class="register_password_strength"
-                onclick={ctx.link().callback(|_| RegisterMsg::RegisterUser)}>
-                { "Register" }
-            </button>
-            <p> {format!("Register error: {:?}", self.error.clone())} </p>
             </div>
-        </div>
+            </div>
         }
     }
 }
