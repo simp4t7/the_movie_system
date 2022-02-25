7e8967068111395edea4cf785084242e9e8ba4c5
t <simp4t7@gmail.com>
2022-02-25 00:19:49 +0000

    register page

--- yew_front/src/pages/register.rs
+++ yew_front/src/pages/register.rs
@@ -21,14 +21,64 @@ pub async fn request_register(url: &str, body: UserInfo) -> Result<()> {
     Ok(())
 }
 
+impl Register {
+    pub fn full_validation_check(&mut self) {
+        self.validate_password_strength();
+        self.validate_email();
+        self.validate_repeated_pass();
+        if self.validation.password_strong
+            && self.validation.email_valid
+            && self.validation.password_match
+        {
+            self.validation.all_valid = true;
+        }
+    }
+    pub fn validate_password_strength(&mut self) {
+        log::info!("checking password strength");
+        let score = zxcvbn(&self.password, &[])
+            .ok()
+            .map(|estimate| estimate.score());
+        self.validation.password_score = score;
+        match score {
+            Some(4) => self.validation.password_strong = true,
+            Some(0) | Some(1) | Some(2) | Some(3) => self.validation.password_strong = false,
+            _ => self.validation.password_strong = false,
+        }
+    }
+    pub fn validate_email(&mut self) {
+        match validator::validate_email(&self.username) {
+            true => self.validation.email_valid = true,
+            false => self.validation.email_valid = false,
+        }
+    }
+    pub fn validate_repeated_pass(&mut self) {
+        match self.password.eq(&self.confirmed_password) {
+            true => self.validation.password_match = true,
+            false => self.validation.password_match = false,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Default)]
+pub struct RegisterValidation {
+    pub all_valid: bool,
+    pub email_valid: bool,
+    pub password_strong: bool,
+    pub password_score: Option<u8>,
+    pub password_match: bool,
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct Register {
-    username: String,
+    pub username: String,
+    pub validation: RegisterValidation,
     pub password: String,
     pub confirmed_password: String,
     pub password_strength: Option<u8>,
     pub error: Option<Error>,
 }
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum RegisterMsg {
     SetUsername(InputEvent),
     SetPassword(InputEvent),
@@ -44,6 +94,7 @@ impl Component for Register {
         log::info!("creating login page");
         Self {
             username: String::new(),
+            validation: RegisterValidation::default(),
             password: String::new(),
             confirmed_password: String::new(),
             password_strength: None,
@@ -59,39 +110,37 @@ impl Component for Register {
                 if let Some(elem) = text.target_dyn_into::<HtmlInputElement>() {
                     self.username = elem.value();
                 }
+                self.validate_email();
             }
             SetPassword(text) => {
                 if let Some(elem) = text.target_dyn_into::<HtmlInputElement>() {
                     self.password = elem.value();
                 }
+                self.validate_password_strength();
             }
             ConfirmPassword(text) => {
                 if let Some(elem) = text.target_dyn_into::<HtmlInputElement>() {
                     self.confirmed_password = elem.value();
-                    self.password_strength = zxcvbn(&self.password, &[])
-                        .ok()
-                        .map(|estimate| estimate.score());
                 }
+                self.validate_repeated_pass();
             }
             RegisterUser => {
                 let username = UserInfo {
                     username: self.username.clone(),
                     password: self.password.clone(),
                 };
-                //if !self.repeated_password_matching() {
-                //self.error = Some(Error::PasswordNotMatchError);
-                //} else if self.password_strength < Some(3) {
-                //self.error = Some(Error::PasswordWeakError);
-                //} else {
-                //self.error = None;
-                ctx.link().send_future(async move {
-                    let resp = request_register(&REGISTER_URL, username).await;
-                    match resp {
-                        Ok(_a) => log::info!("success!"),
-                        Err(e) => log::info!("oh no, {:?}", &e),
-                    }
-                    RegisterMsg::Noop
-                });
+
+                self.full_validation_check();
+                if self.validation.all_valid {
+                    ctx.link().send_future(async move {
+                        let resp = request_register(&REGISTER_URL, username).await;
+                        match resp {
+                            Ok(_a) => log::info!("success!"),
+                            Err(e) => log::info!("oh no, {:?}", &e),
+                        }
+                        RegisterMsg::Noop
+                    });
+                }
                 //}
             }
         }
--- yew_front/src/pages/register_html.rs
+++ yew_front/src/pages/register_html.rs
@@ -4,29 +4,29 @@ use yew::prelude::*;
 use zxcvbn::zxcvbn;
 
 impl Register {
-    fn get_password_strength_estimate(&self) -> Option<u8> {
-        zxcvbn(&self.password, &[])
-            .ok()
-            .map(|estimate| estimate.score())
-    }
-    fn get_password_strength_text(&self) -> String {
-        if self.password.is_empty() {
-            return "Provide a password".to_string();
-        }
-        format!(
-            "Password Strength = {:?}",
-            self.get_password_strength_estimate().unwrap()
-        )
-    }
-    pub fn repeated_password_matching(&self) -> bool {
-        self.password.eq(&self.confirmed_password)
-    }
-    pub fn repeated_password_matching_text(&self) -> String {
-        format!(
-            "Repeat password correct: {:?}",
-            self.repeated_password_matching()
-        )
-    }
+    //fn get_password_strength_estimate(&self) -> Option<u8> {
+    //zxcvbn(&self.password, &[])
+    //.ok()
+    //.map(|estimate| estimate.score())
+    //}
+    //fn get_password_strength_text(&self) -> String {
+    //if self.password.is_empty() {
+    //return "Provide a password".to_string();
+    //}
+    //format!(
+    //"Password Strength = {:?}",
+    //self.get_password_strength_estimate().unwrap()
+    //)
+    //}
+    //pub fn repeated_password_matching(&self) -> bool {
+    //self.password.eq(&self.confirmed_password)
+    //}
+    //pub fn repeated_password_matching_text(&self) -> String {
+    //format!(
+    //"Repeat password correct: {:?}",
+    //self.repeated_password_matching()
+    //)
+    //}
     pub fn register_html(&self, ctx: &Context<Self>) -> Html {
         html! {
             <div class="has-background-grey-lighter">
@@ -45,19 +45,51 @@ impl Register {
                 </div>
                 <div class="container field column is-9">
                   <p class="control has-icons-left">
-                    <input class="input" type="password" placeholder="Password"
-                    maxlength=50
-                    oninput={ctx.link().callback(RegisterMsg::SetPassword)}/>
+
+                  { match self.validation.password_strong {
+                        true => html!{
+                            <input class="input is-primary" type="password" placeholder="Password"
+                            maxlength=50
+                            oninput={ctx.link().callback(RegisterMsg::SetPassword)}/>
+                        },
+                        false => html!{<input class="input" type="password" placeholder="Password"
+                            maxlength=50
+                            oninput={ctx.link().callback(RegisterMsg::SetPassword)}/>
+                        },
+                        }
+                  }
                     <span class="icon is-small is-left">
                     <i class="material-icons">{"lock"}</i>
                     </span>
                     </p>
                 </div>
                 <div class="container field column is-9">
+                    {
+                    match self.validation.password_score {
+                        Some(0) => html!{<progress class="progress is-danger" value="0" max="100"></progress>},
+                        Some(1) => html!{<progress class="progress is-danger" value="25" max="100"></progress>},
+                        Some(2) => html!{<progress class="progress is-warning" value="50" max="100"></progress>},
+                        Some(3) => html!{<progress class="progress is-warning" value="75" max="100"></progress>},
+                        Some(4) => html!{<progress class="progress is-primary" value="100" max="100"></progress>},
+                        _ => html!{<progress class="progress is-primary" max="100"></progress>},
+
+                    }
+                }
+                </div>
+                <div class="container field column is-9">
                   <p class="control has-icons-left">
-                    <input class="input" type="password" placeholder="Repeat Password"
-                    maxlength=50
-                    oninput={ctx.link().callback(RegisterMsg::ConfirmPassword)}/>
+                  { match self.validation.password_match {
+                        true => html!{
+                            <input class="input is-primary" type="password" placeholder="Repeat Password"
+                            maxlength=50
+                            oninput={ctx.link().callback(RegisterMsg::ConfirmPassword)}/>
+                        },
+                        false => html!{<input class="input" type="password" placeholder="Repeat Password"
+                            maxlength=50
+                            oninput={ctx.link().callback(RegisterMsg::ConfirmPassword)}/>
+                        },
+                        }
+                  }
                     <span class="icon is-small is-left">
                     <i class="material-icons">{"lock"}</i>
                     </span>
@@ -72,10 +104,10 @@ impl Register {
                 </div>
                 <div class="container column is-6 is-centered">
                 </div>
-
-                <p> {self.get_password_strength_text()} </p>
-                <p> {self.repeated_password_matching_text()} </p>
-                <p> {format!("Register error: {:?}", self.error.clone())} </p>
+                <p> {format!("email valid: {:?}", self.validation.email_valid)} </p>
+                <p> {format!("password_strong: {:?}", self.validation.password_strong)} </p>
+                <p> {format!("password strength: {:?}", self.validation.password_score)} </p>
+                <p> {format!("correctly repeated password: {:?}", self.validation.password_match)} </p>
             </div>
             </div>
             </div>
